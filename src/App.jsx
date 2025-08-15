7e3cca5bbdaab07344a2d231d2a70a1987421fe6
shard-git <shardulmakrampure@gmail.com>
2025-08-16 00:47:53 +0530

    I uploaded project on github 1st time

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './components/Home'
+
+export default function App () {
+  return (
+    <div>
+      <Home/>
+      <h1>shardz</h1>
+    </div>
+  )
+}
+
